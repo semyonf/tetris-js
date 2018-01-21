1c565a1e5e8183e0a8ad91fded593ee393e17a18
Semyon Fomin <taxnuke@gmail.com>
2018-01-21 16:32:01 +0300

    Improve brick style

--- Tetris.js
+++ Tetris.js
@@ -675,15 +675,49 @@
     this.rgb = rgb;
     this.draw = () => {
       context.fillStyle = this.rgb;
-      context.fillRect(
-        this.x,
-        this.y,
-        brickSize - 1,
-        brickSize - 1
-      );
+      context.beginPath();
+      context.moveTo(this.x, this.y);
+      context.lineTo(this.x + brickSize - 1, this.y);
+      context.lineTo(this.x, this.y + brickSize - 1);
+      context.closePath();
+      context.fill();
+
+      context.fillStyle = modifyRgb(this.rgb, 0.9);
+      context.beginPath();
+      context.moveTo(this.x + brickSize - 1, this.y);
+      context.lineTo(this.x, this.y + brickSize - 1);
+      context.lineTo(this.x, this.y + brickSize - 1);
+      context.lineTo(this.x + brickSize - 1, this.y + brickSize - 1);
+      context.closePath();
+      context.fill();
     };
 
     return this;
+  }
+
+  /**
+   * A function to darken or lighten rgb color strings
+   * @param {string} color
+   * @param {number} factor
+   * @returns {string}
+   */
+  function modifyRgb(color, factor) {
+    const
+      regexp = /rgb\((\d+),(\d+),(\d+)\)/g,
+      matches = regexp.exec(color);
+
+    let
+      colors = [
+        matches[1],
+        matches[2],
+        matches[3]
+      ];
+
+    colors.forEach(function(color, index, colors) {
+      colors[index] = Math.floor(color * factor);
+    });
+
+    return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
   }
 
   /**
