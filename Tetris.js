d80420eed109986914867899bb9289628d9926f7
Semyon Fomin <taxnuke@gmail.com>
2018-07-01 18:59:22 +0300

    Encapsulate Joystick

--- Tetris.js
+++ Tetris.js
@@ -1,26 +1,4 @@
-/**
- * MIT License
- *
- * Copyright (c) 2018 Semyon Fomin
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+import Joystick from './Joystick.js';
 
 (function (undefined) {
   'use strict';
@@ -133,62 +111,7 @@
     };
   })();
 
-  const joystick = (() => {
-    const keyStates = Object.seal(
-      Object.assign({
-        Escape: false,
-        Enter: false,
-        anyKey: false
-      }, keyMap)
-    );
-
-    Object.keys(keyStates).forEach(keyState => keyState = false);
-
-    const callbacks = {}, keyQueue = [];
-
-    function keyEvents(e) {
-      const isDown = (e.type === 'keydown'), keyCode = e.code;
-      keyStates.anyKey = isDown;
-
-      if (isDown && callbacks.anyKey !== undefined) {
-        callbacks.anyKey(keyCode);
-      }
-
-      if (keyStates[keyCode] !== undefined) {
-        e.preventDefault();
-        keyStates[keyCode] = isDown;
-
-        if (isDown) {
-          if (keyCode in keyMap) {
-            keyQueue.push(keyCode);
-          }
-
-          if (callbacks[keyCode] !== undefined) {
-            callbacks[keyCode]();
-          }
-        }
-      }
-    }
-
-    /**
-     * Public interface
-     */
-    return {
-      keys: keyStates,
-      keyQueue,
-      start() {
-        addEventListener('keyup', keyEvents);
-        addEventListener('keydown', keyEvents);
-      },
-      stop() {
-        removeEventListener('keyup', keyEvents);
-        removeEventListener('keydown', keyEvents);
-      },
-      setCallback(key, callback) {
-        callbacks[key] = callback;
-      }
-    };
-  })();
+  const joystick = new Joystick(keyMap);
 
   const game = (() => {
     let
