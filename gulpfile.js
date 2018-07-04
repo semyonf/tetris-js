24b7faecf8970eff21ce1ecc619cc95ff4162d76
Semyon Fomin <taxnuke@gmail.com>
2018-07-04 22:00:23 +0300

    Make use of gulp-header-comment and move .babelrc to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,17 @@
 const gulp = require('gulp');
-const header = require('gulp-header');
+const path = require('path');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
-
-const copyright = `\
-/**
- * MIT License
- *
- * Copyright (c) 2018 Semyon Fomin
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */\n\n`;
+const clean = require('gulp-clean');
+const headerComment = require('gulp-header-comment');
 
 gulp.task('default', () => {
   gulp.src('dist/tetris.js')
-    .pipe(header(copyright))
+    .pipe(headerComment({
+      file: path.join(__dirname, 'LICENSE')
+    }))
+    // .pipe(header(copyright))
     .pipe(gulp.dest('dist'))
 
     .pipe(babel({
@@ -43,4 +23,9 @@ gulp.task('default', () => {
     .pipe(uglify())
     .pipe(rename('tetris.es5.min.js'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src('dist/*', { read: false })
+    .pipe(clean());
 });
