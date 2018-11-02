474d36b51dcee37a6a7077c6e9d1c0cbe594fcd7
Semyon Fomin <taxnuke@gmail.com>
2018-11-02 16:26:55 +0300

    Fix IGameConfig

--- src/IGameConfig.ts
+++ src/IGameConfig.ts
@@ -1,10 +1,10 @@
 export default interface IGameConfig {
-  context: CanvasRenderingContext2D,
+  context?: CanvasRenderingContext2D,
   debug: boolean | undefined
   board: {
     boardWidth: number,
     boardHeight: number,
     brickSize: number
   },
-  customControls: object[]
+  customControls?: object[]
 }
