2dc56627ccb65a9791b6aa8b78ca2325a26bc914
Semyón <semyonf@icloud.com>
2023-07-01 00:59:48 +0200

    refactor: rotation logic

--- src/shape/commands/rotate-command.ts
+++ src/shape/commands/rotate-command.ts
@@ -4,25 +4,24 @@ import Shape from '../shape';
 
 export default class RotateCommand implements ShapeCommand {
   public execute(this: Shape, board: Board) {
-    const temp = new Shape(this);
+    const potentialShape = new Shape(this);
 
-    if (Shape.parameters.types[temp.type].name !== 'O') {
-      temp.orientation = temp.orientation === 3 ? 0 : ++temp.orientation;
-      temp.applyOrientation();
-
-      for (const brick of temp.bricks) {
-        for (const staticBrick of board.staticBricks) {
-          if (brick.x === staticBrick.x && brick.y === staticBrick.y) {
-            return;
-          }
-        }
-
-        if (brick.x >= board.width || brick.x <= 0 || brick.y >= board.height) {
-          return;
-        }
-      }
+    if (this.radialSymmetry) {
+      return;
     }
 
-    board.activeShape = temp;
+    potentialShape.rotate();
+
+    const collisions = potentialShape.checkCollisions(
+      board.staticBricks,
+      board.width,
+      board.height,
+    );
+
+    if (Object.values(collisions).some((v) => v)) {
+      return;
+    }
+
+    board.activeShape = potentialShape;
   }
 }
--- src/shape/shape.ts
+++ src/shape/shape.ts
@@ -2,115 +2,134 @@ import Brick from '../brick';
 import ParkMiller from 'park-miller';
 import { Collisions, Sides, sides } from '../board';
 
-export default class Shape {
-  public static parameters = {
-    colors: [
-      { name: 'orange', rgb: 'rgb(239,108,0)' },
-      { name: 'red', rgb: 'rgb(211,47,47)' },
-      { name: 'green', rgb: 'rgb(76,175,80)' },
-      { name: 'blue', rgb: 'rgb(33,150,243)' },
-      { name: 'yellow', rgb: 'rgb(255,235,59)' },
-      { name: 'cyan', rgb: 'rgb(0,188,212)' },
-      { name: 'pink', rgb: 'rgb(233,30,99)' },
-      { name: 'white', rgb: 'rgb(224,224,224)' },
-    ],
-    orientations: [
-      {
-        angle: 0,
-        matrix: [
-          [1, 0],
-          [0, 1],
-        ],
-      },
-      {
-        angle: 90,
-        matrix: [
-          [0, -1],
-          [1, 0],
-        ],
-      },
-      {
-        angle: 180,
-        matrix: [
-          [-1, 0],
-          [0, -1],
-        ],
-      },
-      {
-        angle: 270,
-        matrix: [
-          [0, 1],
-          [-1, 0],
-        ],
-      },
-    ],
-    types: [
-      {
-        name: 'I',
-        matrix: [
-          [0, -1],
-          [0, 1],
-          [0, 2],
-        ],
-      },
-      {
-        name: 'O',
-        matrix: [
-          [0, 1],
-          [1, 0],
-          [1, 1],
-        ],
-      },
-      {
-        name: 'Z',
-        matrix: [
-          [0, -1],
-          [-1, 0],
-          [1, -1],
-        ],
-      },
-      {
-        name: 'S',
-        matrix: [
-          [-1, -1],
-          [0, -1],
-          [1, 0],
-        ],
-      },
-      {
-        name: 'T',
-        matrix: [
-          [1, 0],
-          [-1, 0],
-          [0, 1],
-        ],
-      },
-      {
-        name: 'J',
-        matrix: [
-          [1, 0],
-          [-1, 0],
-          [-1, 1],
-        ],
-      },
-      {
-        name: 'L',
-        matrix: [
-          [1, 0],
-          [-1, 0],
-          [-1, -1],
-        ],
-      },
-    ],
-  } as const;
+const shapeColors: {
+  name: string;
+  rgb: `rgb(${number},${number},${number})`;
+}[] = [
+  { name: 'orange', rgb: 'rgb(239,108,0)' },
+  { name: 'red', rgb: 'rgb(211,47,47)' },
+  { name: 'green', rgb: 'rgb(76,175,80)' },
+  { name: 'blue', rgb: 'rgb(33,150,243)' },
+  { name: 'yellow', rgb: 'rgb(255,235,59)' },
+  { name: 'cyan', rgb: 'rgb(0,188,212)' },
+  { name: 'pink', rgb: 'rgb(233,30,99)' },
+  { name: 'white', rgb: 'rgb(224,224,224)' },
+];
 
+const shapeOrientations: Array<{ angle: number; matrix: number[][] }> = [
+  {
+    angle: 0,
+    matrix: [
+      [1, 0],
+      [0, 1],
+    ],
+  },
+  {
+    angle: 90,
+    matrix: [
+      [0, -1],
+      [1, 0],
+    ],
+  },
+  {
+    angle: 180,
+    matrix: [
+      [-1, 0],
+      [0, -1],
+    ],
+  },
+  {
+    angle: 270,
+    matrix: [
+      [0, 1],
+      [-1, 0],
+    ],
+  },
+];
+
+const shapeTypes: Array<{
+  name: string;
+  radiallySymmetrical: boolean;
+  matrix: number[][];
+}> = [
+  {
+    name: 'I',
+    radiallySymmetrical: false,
+    matrix: [
+      [0, -1],
+      [0, 1],
+      [0, 2],
+    ],
+  },
+  {
+    name: 'O',
+    radiallySymmetrical: true,
+    matrix: [
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+  },
+  {
+    name: 'Z',
+    radiallySymmetrical: false,
+    matrix: [
+      [0, -1],
+      [-1, 0],
+      [1, -1],
+    ],
+  },
+  {
+    name: 'S',
+    radiallySymmetrical: false,
+    matrix: [
+      [-1, -1],
+      [0, -1],
+      [1, 0],
+    ],
+  },
+  {
+    name: 'T',
+    radiallySymmetrical: false,
+    matrix: [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+    ],
+  },
+  {
+    name: 'J',
+    radiallySymmetrical: false,
+    matrix: [
+      [1, 0],
+      [-1, 0],
+      [-1, 1],
+    ],
+  },
+  {
+    name: 'L',
+    radiallySymmetrical: false,
+    matrix: [
+      [1, 0],
+      [-1, 0],
+      [-1, -1],
+    ],
+  },
+];
+
+export default class Shape {
   public bricks: Brick[] = [];
   public isFrozen = false;
-  public orientation: number;
+  // 1 rotation = 90 degrees
+  public rotations: number;
   public type: number;
   private startX: number;
   private startY: number;
   private color: number;
+
+  get radialSymmetry() {
+    return shapeTypes[this.type].radiallySymmetrical;
+  }
 
   constructor(sourceShape: Shape);
   constructor(boardWidth: number, brickSize: number, random: ParkMiller);
@@ -146,38 +165,43 @@ export default class Shape {
     return collisions;
   }
 
+  rotate() {
+    this.rotations++;
+    this.applyOrientation();
+  }
+
   public applyOrientation() {
-    const type = Shape.parameters.types[this.type].matrix;
-    const orientation = Shape.parameters.orientations[this.orientation].matrix;
-    const oriented = this.getRotatedShape(type, orientation);
+    const type = shapeTypes[this.type].matrix;
+    const orientation = shapeOrientations[this.rotations % 4].matrix;
+    const orientedShape = this.getOrientedShape(type, orientation);
 
     const center = this.bricks[0];
 
     for (let i = 0; i < 3; ++i) {
-      this.bricks[i + 1].x = center.x + oriented[i][0] * this.startY;
-      this.bricks[i + 1].y = center.y + oriented[i][1] * this.startY;
+      this.bricks[i + 1].x = center.x + orientedShape[i][0] * this.startY;
+      this.bricks[i + 1].y = center.y + orientedShape[i][1] * this.startY;
     }
 
     return this;
   }
 
-  private getRotatedShape(
-    shape: typeof Shape.parameters.types[number]['matrix'],
-    rotation: typeof Shape.parameters.orientations[number]['matrix'],
+  private getOrientedShape(
+    shape: typeof shapeTypes[number]['matrix'],
+    rotation: typeof shapeOrientations[number]['matrix'],
   ) {
-    const rotatedShape: number[][] = [];
+    const orientedShape: number[][] = [];
 
     for (let i = 0; i < 3; ++i) {
-      rotatedShape[i] = [];
+      orientedShape[i] = [];
       for (let j = 0; j < 2; ++j) {
-        rotatedShape[i][j] = 0;
+        orientedShape[i][j] = 0;
         for (let k = 0; k < 2; ++k) {
-          rotatedShape[i][j] += shape[i][k] * rotation[k][j];
+          orientedShape[i][j] += shape[i][k] * rotation[k][j];
         }
       }
     }
 
-    return rotatedShape;
+    return orientedShape;
   }
 
   private _defaultConstructor(
@@ -187,19 +211,16 @@ export default class Shape {
   ) {
     this.startX = boardWidth / 2;
     this.startY = brickSize;
-    this.color = random.integerInRange(0, Shape.parameters.colors.length - 1);
-    this.type = random.integerInRange(0, Shape.parameters.types.length - 1);
-    this.orientation = random.integerInRange(
-      0,
-      Shape.parameters.orientations.length - 1,
-    );
+    this.color = random.integerInRange(0, shapeColors.length - 1);
+    this.type = random.integerInRange(0, shapeTypes.length - 1);
+    this.rotations = random.integerInRange(0, shapeOrientations.length - 1);
 
     for (let i = 0; i < 4; ++i) {
       this.bricks.push(
         new Brick(
           this.startX,
           this.startY,
-          Shape.parameters.colors[this.color].rgb,
+          shapeColors[this.color].rgb,
           brickSize,
         ),
       );
@@ -213,7 +234,7 @@ export default class Shape {
     this.type = sourceShape.type;
     this.startX = sourceShape.startX;
     this.startY = sourceShape.startY;
-    this.orientation = sourceShape.orientation;
+    this.rotations = sourceShape.rotations;
 
     for (let i = 0; i < 4; ++i) {
       this.bricks.push(new Brick(sourceShape.bricks[i]));
